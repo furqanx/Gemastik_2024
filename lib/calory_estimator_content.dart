a7dfaaa21662739f62ee0f8d1bc0f41ee3e7d7da
MDanishRabbani <danishrabbani1806@gmail.com>
2024-05-14 10:18:46 +0700

    feat: add card capture image result

--- lib/calory_estimator_content.dart
+++ lib/calory_estimator_content.dart
@@ -1,3 +1,4 @@
+import 'dart:io';
 import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
 
@@ -15,6 +16,7 @@ class _CaloryEstimatorContentState extends State<CaloryEstimatorContent> {
   late Future<void> _initializeControllerFuture;
   bool _flashOn = false;
   double _zoomLevel = 1.0;
+  String? _capturedImagePath;
 
   @override
   void initState() {
@@ -49,60 +51,98 @@ class _CaloryEstimatorContentState extends State<CaloryEstimatorContent> {
     });
   }
 
+  Future<void> _captureImage() async {
+    try {
+      await _initializeControllerFuture;
+      final image = await _controller.takePicture();
+      setState(() {
+        _capturedImagePath = image.path;
+      });
+      print('Photo taken at path: ${image.path}');
+    } catch (e) {
+      print('Error capturing photo: $e');
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          FutureBuilder<void>(
-            future: _initializeControllerFuture,
-            builder: (context, snapshot) {
-              if (snapshot.connectionState == ConnectionState.done) {
-                return CameraPreview(_controller);
-              } else {
-                return CircularProgressIndicator();
-              }
-            },
-          ),
-          SizedBox(height: 20),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              IconButton(
-                icon: Icon(Icons.zoom_in, size: 30),
-                onPressed: _toggleZoom,
-              ),
-              SizedBox(width: 20),
-              GestureDetector(
-                onTap: () async {
-                  try {
-                    await _initializeControllerFuture;
-                    final image = await _controller.takePicture();
-                    // Do something with the captured image
-                    print('Photo taken at path: ${image.path}');
-                  } catch (e) {
-                    print('Error capturing photo: $e');
-                  }
-                },
-                child: Container(
-                  width: 60,
-                  height: 60,
-                  decoration: BoxDecoration(
-                    color: Colors.red,
-                    shape: BoxShape.circle,
-                  ),
-                  child: Icon(Icons.camera, color: Colors.white, size: 30),
+    return Scaffold(
+      appBar: _capturedImagePath != null
+          ? AppBar(
+        title: Text('Captured Image'),
+        leading: IconButton(
+          icon: Icon(Icons.arrow_back),
+          onPressed: () {
+            setState(() {
+              _capturedImagePath = null;
+            });
+          },
+        ),
+      )
+          : null,
+      body: Center(
+        child: _capturedImagePath == null
+            ? Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            FutureBuilder<void>(
+              future: _initializeControllerFuture,
+              builder: (context, snapshot) {
+                if (snapshot.connectionState == ConnectionState.done) {
+                  return CameraPreview(_controller);
+                } else {
+                  return CircularProgressIndicator();
+                }
+              },
+            ),
+            SizedBox(height: 20),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                IconButton(
+                  icon: Icon(Icons.zoom_in, size: 30),
+                  onPressed: _toggleZoom,
                 ),
-              ),
-              SizedBox(width: 20),
-              IconButton(
-                icon: Icon(_flashOn ? Icons.flash_on : Icons.flash_off, size: 30),
-                onPressed: _toggleFlash,
+                SizedBox(width: 20),
+                GestureDetector(
+                  onTap: _captureImage,
+                  child: Container(
+                    width: 60,
+                    height: 60,
+                    decoration: BoxDecoration(
+                      color: Colors.red,
+                      shape: BoxShape.circle,
+                    ),
+                    child: Icon(Icons.camera, color: Colors.white, size: 30),
+                  ),
+                ),
+                SizedBox(width: 20),
+                IconButton(
+                  icon: Icon(_flashOn ? Icons.flash_on : Icons.flash_off, size: 30),
+                  onPressed: _toggleFlash,
+                ),
+              ],
+            ),
+          ],
+        )
+            : Card(
+          margin: EdgeInsets.all(16),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Image.file(File(_capturedImagePath!)),
+              SizedBox(height: 20),
+              ElevatedButton(
+                onPressed: () {
+                  setState(() {
+                    _capturedImagePath = null;
+                  });
+                },
+                child: Text('Back to Camera'),
               ),
             ],
           ),
-        ],
+        ),
       ),
     );
   }
